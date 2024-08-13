09d0ebb2c63b1729b99a9f242d907efb03957c56
양재혁 <yjh5128@naver.com>
2024-08-13 17:41:20 +0900

    test push

---
+++ src/component/mainIndex/IndexMain.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function IndexMain() {
+  return (
+    <div className="outer_wrapper">
+      <div className="inner_wrapper">
+        <img src="../../imgResources/datie_background.webp" alt="Background" />
+      </div>
+    </div>
+  );
+}
+
+export default IndexMain;
