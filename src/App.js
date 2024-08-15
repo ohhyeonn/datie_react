21e64031a19938bdcbfceb33d167b106b9c6e088
kon <kimgo816@naver.com>
2024-08-15 15:23:16 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import { Routes, Route } from "react-router-dom";
-import Paypassword from "./component/pay/Paypassword";
-import Payresult from "./component/pay/Payresult";
+import RealHeader from "./component/RealHeader"
+import Header from "./component/Header"
+import Footer from "./component/Footer"
 
 function App() {
   return (
-    <Routes>
-      <Route path="/pay/Paypassword" element={<Paypassword />} />
-      <Route path="/pay/Payresult" element={<Payresult />} />
-    </Routes>
+    <div>
+    <div className="App">
+      <RealHeader/>
+      <Header title={"서브 헤더"}/>
+    </div>
+    <div className="footer">
+      <Footer/> 
+    </div>
+    </div>
   );
 }
 
