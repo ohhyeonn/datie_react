9628f9d8ac80eefebdd3a8e1f265ac3edf403f3d
양재혁 <yjh5128@naver.com>
2024-08-16 15:01:52 +0900

    메인화면(버튼연결전)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 import Paypassword from "./component/pay/Paypassword";
+import IndexMain from "./component/mainIndex/IndexMain";
 
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<IndexMain />} />
       <Route path="/pay/Paypassword" element={<Paypassword />} />
     </Routes>
   );
