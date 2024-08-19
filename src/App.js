ca680297a2addb140755e2438ce7cbbe0a848ae9
ohhyeonn <ohhyeon120@gmail.com>
2024-08-19 15:58:56 +0900

    관리자 페이지 front 개발

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import { Routes, Route } from "react-router-dom";
 import Paypassword from "./component/pay/Paypassword";
 import IndexMain from "./component/mainIndex/IndexMain";
 import Payresult from "./component/pay/Payresult";
-
+import AdminMember from "./component/admin/AdminMember";
+import AdminStatistics from "./component/admin/AdminStatistics";
 function App() {
   return (
     <Routes>
       <Route path="/" element={<IndexMain />} />
       <Route path="/pay/Paypassword" element={<Paypassword />} />
       <Route path="/pay/Payresult" element={<Payresult />} />
+      <Route path="/admin/member" element={<AdminMember />} />
+      <Route path="/admin" element={<AdminStatistics />} />
     </Routes>
   );
 }
