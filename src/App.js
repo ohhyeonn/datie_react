137f329e2d2dd5427336bde88213f68065029f83
양재혁 <yjh5128@naver.com>
2024-08-20 12:04:30 +0900

    테스트를 위한 수정

--- src/App.js
+++ src/App.js
@@ -13,6 +13,15 @@ import IndexMain from './component/mainIndex/IndexMain';
 import Payresult from './component/pay/Payresult';
 import AdminMember from './component/admin/AdminMember';
 import AdminStatistics from './component/admin/AdminStatistics';
+import CardPasswordChange from './component/profile/CardPasswordChange'; // 경로 확인 후 수정
+import CardLostReport from './component/profile/CardLostReport'; // 새로 추가된 컴포넌트 경로
+import CardLostReportCancellation from './component/profile/CardLostReportCancellation'; // 새로 추가된 컴포넌트 경로
+import CardCancellation from './component/profile/CardCancellation'; // 새로 추가된 컴포넌트 경로
+import EditProfile from './component/profile/EditProfile'; // 경로 확인 후 수정
+import ViewProfile from './component/profile/ViewProfile'; // 경로 확인 후 수정
+import DiaryDetail from './component/diary/pages/DiaryDetail';
+import DiaryHome from './component/diary/pages/DiaryHome';
+
 function App() {
     return (
         <Routes>
@@ -32,12 +41,14 @@ function App() {
             <Route path="/pay/PayInfo" element={<PayInfo />} />
             <Route path="/admin/member" element={<AdminMember />} />
             <Route path="/admin" element={<AdminStatistics />} />
-            {/* <Route path="/" element={<SignUpComponent />} /> */}
-            {/* <Route path="/" element={<SignUpForm />} /> */}
-            {/* <Route path="/" element={<CardCreationForm />} /> */}
-            {/* <Route path="/" element={<ProfileComparison />} /> */}
-            {/* <Route path="/" element={<CardInfoInput />} /> */}
-            {/* <Route path="/" element={<CardApplicationComplete />} /> */}
+            <Route path="/card-lost-report" element={<CardLostReport />} />
+            <Route path="/card-lost-report-cancellation" element={<CardLostReportCancellation />} />
+            <Route path="/card-cancellation" element={<CardCancellation />} />
+            <Route path="/change-cardpassword" element={<CardPasswordChange />} />
+            <Route path="/edit-profile/:email" element={<EditProfile />} />
+            <Route path="/view-profile/:email" element={<ViewProfile />} />
+            <Route path="/diary" element={<DiaryHome />} />
+            <Route path="/diary/detail" element={<DiaryDetail />} />
             <Route path="/" element={<LoginPage />} />
         </Routes>
     );
