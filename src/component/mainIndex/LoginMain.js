a3024f983327ecf7ee67bff78e6f0165a1652c49
양재혁 <yjh5128@naver.com>
2024-08-25 21:09:30 +0900

    240825 로그인 후 화면

--- src/component/mainIndex/LoginMain.js
+++ src/component/mainIndex/LoginMain.js
@@ -1,76 +1,255 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Box, Typography, Card, CardContent } from "@mui/material";
+import axios from "axios";
+import { jwtDecode } from "jwt-decode";
+import ResponsiveAppBar from "../RealHeader";
+
+// 이미지 import
+import type1Front from "../../assets/type1-front.png";
+import type2Front from "../../assets/type2-front.png";
+import type3Front from "../../assets/type3-front.png";
+import type1Back from "../../assets/type1-back.png";
+import type2Back from "../../assets/type2-back.png";
+import type3Back from "../../assets/type3-back.png";
 
 function LoginMain() {
-  // 현재 날짜와 과거 날짜를 계산하여 D+일 계산
-  const startDate = new Date("2023-01-01");
-  const today = new Date();
-  const diffTime = Math.abs(today - startDate);
-  const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+  const [cardInfo, setCardInfo] = useState(null); // 카드 정보를 저장할 상태
+  const [paymentRecords, setPaymentRecords] = useState([]); // 결제 기록을 저장할 상태
+  const [ownerName, setOwnerName] = useState(""); // 첫 번째 유저 이름 상태
+  const [partnerName, setPartnerName] = useState(""); // 두 번째 유저 이름 상태
+  const [userno, setUserno] = useState(null); // 디코딩된 userno를 저장할 상태
+  const [isFlipped, setIsFlipped] = useState(false); // 카드가 뒤집혔는지 여부
+  const cardno = 13; // 실제 cardno로 변경하세요
+
+  const token = localStorage.getItem("jwtToken"); // JWT 토큰을 가져오는 부분
+
+  if (token) {
+    const decoded = jwtDecode(token); // JWT 토큰 디코딩
+    console.log(decoded); // 디코딩된 정보 출력
+    setUserno(decoded.userno); // userno를 상태로 설정
+  }
+
+  useEffect(() => {
+    if (cardno) {
+      // 카드 정보 가져오기
+      axios
+        .post(`http://localhost:8090/api/card/${cardno}`)
+        .then((response) => {
+          setCardInfo(response.data); // 가져온 카드 정보를 상태에 저장
+
+          // 첫 번째 유저 이름 가져오기
+          if (response.data.userno) {
+            axios
+              .get(`http://localhost:8090/api/profile?userno=${response.data.userno}`)
+              .then((res) => {
+                setOwnerName(res.data.name); // 첫 번째 유저 이름 설정
+              })
+              .catch((error) => {
+                console.error("Error fetching owner name:", error);
+              });
+          }
+
+          // 두 번째 유저 이름 가져오기
+          if (response.data.userno2) {
+            axios
+              .get(`http://localhost:8090/api/profile?userno=${response.data.userno2}`)
+              .then((res) => {
+                setPartnerName(res.data.name); // 두 번째 유저 이름 설정
+              })
+              .catch((error) => {
+                console.error("Error fetching partner name:", error);
+              });
+          }
+        })
+        .catch((error) => {
+          console.error("Error fetching card info:", error);
+        });
+
+      // 결제 기록 가져오기
+      axios
+        .post(`http://localhost:8090/api/card/${cardno}/payment-records`)
+        .then((response) => {
+          setPaymentRecords(response.data); // 가져온 결제 기록을 상태에 저장
+        })
+        .catch((error) => {
+          console.error("Error fetching payment records:", error);
+        });
+    }
+  }, [cardno]);
+
+  // 날짜 형식 변환 함수
+  const formatDateTime = (timestamp) => {
+    const date = new Date(timestamp);
+    const formattedDate = date.toLocaleDateString("ko-KR", {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+    });
+    const formattedTime = date.toLocaleTimeString("ko-KR", {
+      hour: "2-digit",
+      minute: "2-digit",
+    });
+    return `${formattedDate} ${formattedTime}`;
+  };
+
+  //카드 날짜 변환 함수
+  const formatDateTimeCard = (timestamp) => {
+    const date = new Date(timestamp);
+    const formattedDate = date.toLocaleDateString("ko-KR", {
+      year: "2-digit",
+      month: "2-digit",
+      day: "2-digit",
+    });
+    return `${formattedDate}`;
+  };
+
+  // 카드 타입에 따른 앞면 이미지 설정
+  const getFrontBackgroundImage = () => {
+    switch (cardInfo.cardtypeno) {
+      case 1:
+        return type1Front;
+      case 2:
+        return type2Front;
+      case 3:
+        return type3Front;
+      default:
+        return null;
+    }
+  };
+
+  // 카드 타입에 따른 뒷면 이미지 설정
+  const getBackBackgroundImage = () => {
+    switch (cardInfo.cardtypeno) {
+      case 1:
+        return type1Back;
+      case 2:
+        return type2Back;
+      case 3:
+        return type3Back;
+      default:
+        return null;
+    }
+  };
+
+  // 카드 클릭 핸들러
+  const handleCardClick = () => {
+    setIsFlipped(!isFlipped);
+  };
 
   return (
     <Box
       sx={{
         display: "flex",
         flexDirection: "column",
-        height: "1792px",
-        width: "1024px",
-        margin: "0 auto",
+        height: "100vh", // 뷰포트 전체 높이로 설정
+        maxWidth: "1024px", // 최대 너비 설정
+        width: "100%", // 너비 100%로 설정
+        margin: "0 auto", // 가로 방향 중앙 정렬
         padding: "20px",
         backgroundColor: "#f0f0f0",
         borderRadius: "8px",
+        justifyContent: "center", // 세로 방향 중앙 정렬
+        alignItems: "center", // 가로 방향 중앙 정렬
       }}
     >
+      <ResponsiveAppBar />
       {/* 카드 정보 섹션 */}
       <Box
         sx={{
-          margin: "0 10px 30px 10px",
-          backgroundColor: "#ffffff",
-          borderRadius: "8px",
-          padding: "20px",
           display: "flex",
-          justifyContent: "center", // 수평 중앙 정렬
-          alignItems: "center", // 수직 중앙 정렬
-          minHeight: "200px", // 카드 정보 섹션의 최소 높이 설정
+          justifyContent: "center", // 가로 방향 중앙 정렬
+          alignItems: "center", // 세로 방향 중앙 정렬
+          position: "relative",
+          width: "600px",
+          height: "400px",
+          perspective: "1000px", // 3D 효과를 위한 원근
         }}
+        onClick={handleCardClick} // 클릭 시 카드 전환
       >
-        <Card
-          sx={{
-            maxWidth: "450px",
-            backgroundColor: "#1976d2",
-            color: "white",
-            padding: "20px",
-            borderRadius: "12px",
-            marginRight: "20px",
-            flexGrow: 0,
-          }}
-        >
-          <CardContent>
-            <Typography variant="h6" component="div" sx={{ marginBottom: "10px" }}>
-              카드 번호: 1234 5678 9012 3456
-            </Typography>
-            <Typography variant="body2" component="div" sx={{ marginBottom: "10px" }}>
-              내 이름: 홍길동
-            </Typography>
-            <Typography variant="body2" component="div" sx={{ marginBottom: "10px" }}>
-              상대 이름: 김철수
-            </Typography>
-            <Typography variant="body2" component="div" sx={{ marginBottom: "10px" }}>
-              유효 기간: 12/24
-            </Typography>
-            <Typography variant="body2" component="div">
-              CVC: 123
-            </Typography>
-          </CardContent>
-        </Card>
-        <Box sx={{ textAlign: "left", flexShrink: 0 }}>
-          <Typography variant="body1" sx={{ fontSize: "18px", marginBottom: "10px" }}>
-            2023년 1월 1일부터 D+{diffDays}일이에요!
-          </Typography>
-          <Typography variant="body2" sx={{ fontSize: "16px", marginTop: "10px" }}>
-            와작와작악어 카드 디자인
-          </Typography>
-        </Box>
+        {cardInfo && (
+          <>
+            {/* 카드 앞면 */}
+            <Card
+              sx={{
+                maxWidth: "450px",
+                width: "100%", // 부모 컨테이너에 맞추기 위해 width를 100%로 설정
+                color: "white",
+                borderRadius: "12px",
+                position: "absolute",
+                top: "10%",
+                left: "10%",
+                backgroundImage: `url(${getFrontBackgroundImage()})`,
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+                aspectRatio: "16/9", // 예: 16:9 비율 사용
+                backfaceVisibility: "hidden",
+                transform: isFlipped ? "rotateY(180deg)" : "rotateY(0deg)",
+                transition: "transform 1.5s", // 애니메이션 시간을 1.5초로 설정
+                display: "flex",
+                justifyContent: "flex-end",
+                alignItems: "flex-end",
+                padding: "20px",
+              }}
+            >
+              <Typography
+                variant="h4"
+                component="div"
+                sx={{ 
+                  color: "white",
+                  fontFamily: '"Gamja Flower", cursive',
+                  fontSize: '22px',
+                 }}
+              >
+                {cardInfo.initials}
+              </Typography>
+            </Card>
+
+            {/* 카드 뒷면 */}
+            <Card
+              sx={{
+                maxWidth: "450px",
+                width: "100%",
+                color: "white",
+                borderRadius: "12px",
+                position: "absolute",
+                top: "10%",
+                left: "10%",
+                backgroundImage: `url(${getBackBackgroundImage()})`,
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+                aspectRatio: "16/9",
+                backfaceVisibility: "hidden",
+                transform: isFlipped ? "rotateY(0deg)" : "rotateY(180deg)",
+                transition: "transform 1.5s", // 애니메이션 시간을 1.5초로 설정
+                transformStyle: "preserve-3d",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+                padding: "20px",
+              }}
+            >
+              <Typography variant="body2" component="div" sx={{ 
+                marginBottom: "10px", 
+                marginTop: "100px",
+                fontFamily: '"Gamja Flower", cursive',
+                fontSize: '22px',
+              }}>
+                {ownerName}님과 {partnerName}님의 데이티카드
+              </Typography>
+              <Typography variant="body2" component="div" sx={{ 
+                marginBottom: "10px",
+                fontFamily: '"Gamja Flower", cursive',
+                fontSize: '22px',
+              }}>
+                유효 기간: {formatDateTimeCard(cardInfo.date)}
+              </Typography>
+              <Typography variant="body2" component="div">
+                CVC: {cardInfo.cvc}
+              </Typography>
+            </Card>
+          </>
+        )}
       </Box>
 
       {/* 카드 내역 섹션 */}
@@ -80,42 +259,58 @@ function LoginMain() {
           backgroundColor: "#ffffff",
           borderRadius: "8px",
           padding: "20px",
+          width: "600px",
           margin: "0px 10px",
           overflowY: "auto",
         }}
       >
-        <Typography variant="h5" component="div" gutterBottom>
+        <Typography variant="h5" component="div" gutterBottom
+          sx={{
+            fontFamily: '"Gamja Flower", cursive',
+            fontSize: '22px',
+        }}>
           카드 내역
         </Typography>
-        {/* 카드 내역 리스트 예시 */}
-        {[
-          { date: "2024-01-01", place: "스타벅스", amount: 5000 },
-          { date: "2024-01-02", place: "맥도날드", amount: 7000 },
-          { date: "2024-01-03", place: "아마존", amount: 20000 },
-        ].map((item, index) => (
-          <Box
-            key={index}
-            sx={{
-              border: "1px solid #ccc",
-              borderRadius: "8px",
-              padding: "10px",
-              marginBottom: "10px",
-              maxWidth: "400px",
-              margin: "20px auto",
-              textAlign: "center",
-            }}
-          >
-            <Typography variant="body1" sx={{ marginBottom: "5px" }}>
-              {item.date}
-            </Typography>
-            <Typography variant="body2" sx={{ marginBottom: "5px" }}>
-              {item.place}
-            </Typography>
-            <Typography variant="body2" sx={{ fontWeight: "bold" }}>
-              {item.amount.toLocaleString()} 원
-            </Typography>
-          </Box>
-        ))}
+        {paymentRecords.length > 0 ? (
+          paymentRecords.map((item, index) => (
+            <Box
+              key={index}
+              sx={{
+                border: "1px solid #ccc",
+                borderRadius: "8px",
+                padding: "10px",
+                marginBottom: "10px",
+                maxWidth: "400px",
+                margin: "20px auto",
+                textAlign: "center",
+              }}
+            >
+              <Typography variant="body1" sx={{ 
+                marginBottom: "5px",
+                fontFamily: '"Gamja Flower", cursive',
+                fontSize: '22px',
+              }}>
+                {formatDateTime(item.confirmdate)}
+              </Typography>
+              <Typography variant="body2" sx={{ 
+                marginBottom: "5px",
+                fontFamily: '"Gamja Flower", cursive',
+                fontSize: '22px',
+              }}>
+                {item.content ? item.content : "Unknown"}
+              </Typography>
+              <Typography variant="body2" sx={{ 
+                fontWeight: "bold",
+                fontFamily: '"Gamja Flower", cursive',
+                fontSize: '22px',
+                }}>
+                {item.amount.toLocaleString()} 원
+              </Typography>
+            </Box>
+          ))
+        ) : (
+          <Typography>Loading payment records...</Typography>
+        )}
       </Box>
     </Box>
   );
